78a5c63f6314bda866d51a3782d85769a9e83286
Bernardo Lissarrague <berlissarrague@gmail.com>
2021-10-02 14:05:45 -0300

    span
    commit

--- Tatto/README.md
+++ Tatto/README.md
@@ -1,4 +1,4 @@
-# Tatto
+# ProyectoTatto
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.7.
 
--- Tatto/src/app/app.module.ts
+++ Tatto/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { DescripcionDeServiciosComponent } from './components/descripcion-de-servicios/descripcion-de-servicios.component';
+import { TrabajosEchosComponent } from './components/trabajos-echos/trabajos-echos.component';
+import { TurnosComponent } from './paginas/turnos/turnos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DescripcionDeServiciosComponent,
+    TrabajosEchosComponent,
+    TurnosComponent,
+    FooterComponent,
+    HeaderComponent
+  
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
